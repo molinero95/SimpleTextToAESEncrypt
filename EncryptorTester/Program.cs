eaca477e1b58702cd4ad0ccb980cc222d1ee441c
molinero95 <jaime.molinero.lacave@gmail.com>
2019-02-01 11:24:43 +0100

    Initial commit

---
+++ EncryptorTester/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using TextToAESEncryptor;
+
+namespace EncryptorTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EncryptMenu();
+            //DecryptMenu();
+            EncryptAndDecryptMenu();
+            Console.ReadKey();
+        }
+
+        public static void EncryptAndDecryptMenu()
+        {
+            Console.WriteLine("Write text: ");
+            string text = Console.ReadLine();
+            Console.WriteLine("Write encrypt password: ");
+            string password = Console.ReadLine();
+            string encryptedText = TextAESEncryptor.TextEncrypt(text, password);
+            Console.WriteLine(encryptedText);
+            string decryptedText = TextAESEncryptor.TextDecrypt(encryptedText, password);
+            Console.WriteLine("Text Written: " + decryptedText); ;
+        }
+
+        public static void EncryptMenu()
+        {
+            Console.WriteLine("Write text: ");
+            string text = Console.ReadLine();
+            Console.WriteLine("Write encrypt password: ");
+            string password = Console.ReadLine();
+            string encryptedText = TextAESEncryptor.TextEncrypt(text, password);
+            Console.WriteLine(encryptedText);
+        }
+
+        public static void DecryptMenu()
+        {
+            Console.WriteLine("Write text: ");
+            string encryptedText = Console.ReadLine();
+            Console.WriteLine("Write encrypt password: ");
+            string password = Console.ReadLine();
+            string decryptedText = TextAESEncryptor.TextDecrypt(encryptedText, password);
+            Console.WriteLine("Text Written: " + decryptedText); ;
+        }
+
+    }
+}
---
+++ TextToAESEncryptor/TextAESEncryptor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace TextToAESEncryptor
+{
+    public static class TextAESEncryptor
+    {
+        public static string TextEncrypt(string text, string encryptPassword)
+        {
+            byte[] textAsByteArray = Encoding.UTF8.GetBytes(text.Trim());
+            byte[] textEncryptedByteArray;
+            using (MemoryStream plainTextMemoryStream = new MemoryStream(textAsByteArray))
+            {
+                using(MemoryStream encryptedTextMemoryStream = new MemoryStream())
+                {
+                    SharpAESCrypt.SharpAESCrypt.Encrypt(encryptPassword, plainTextMemoryStream, encryptedTextMemoryStream);
+                    textEncryptedByteArray = encryptedTextMemoryStream.ToArray();
+                }
+            }
+            return Convert.ToBase64String(textEncryptedByteArray);
+        }
+
+        public static string TextDecrypt(string encryptedText, string encryptPassword)
+        {
+            byte[] textAsByteArray = Convert.FromBase64String(encryptedText);
+            byte[] textEncryptedByteArray;
+            using (MemoryStream plainTextMemoryStream = new MemoryStream(textAsByteArray))
+            {
+                using (MemoryStream encryptedTextMemoryStream = new MemoryStream())
+                {
+                    SharpAESCrypt.SharpAESCrypt.Decrypt(encryptPassword, plainTextMemoryStream, encryptedTextMemoryStream);
+                    textEncryptedByteArray = encryptedTextMemoryStream.ToArray();
+                }
+            }
+            return System.Text.Encoding.UTF8.GetString(textEncryptedByteArray);
+        }
+    }
+}
